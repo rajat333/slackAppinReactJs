dedfbc0056c827bb301332147bad9dd28aa562d0
Rajat Khurana <rajatkhuranak@gmail.com>
2018-11-04 15:22:27 +0530

    Added Routing and other components

--- src/index.js
+++ src/index.js
@@ -4,7 +4,26 @@ import './index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Login from './components/Auth/Login';
+import Register from './components/Auth/Register';
+
+const Root = ()=>(
+
+    <Router>
+        <Switch>
+            <Route exact  path= { '/' } component= {  App } />
+            <Route exact  path= { '/login' } component= {  Login } />
+            <Route exact  path= { '/register' } component= {  Register } />
+
+        </Switch>
+
+    </Router>
+)
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(<Root />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
