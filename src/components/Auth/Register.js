910c034b596f63fd232627873424e17d7fdd36d1
Rajat Khurana <rajatkhuranak@gmail.com>
2018-11-11 03:25:04 +0530

    Added firebase auth to register user

--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -2,15 +2,52 @@ import React from 'react';
 import { Grid, Icon, Form , Segment, Button, Header, Message } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 
+import firebase from "../firebase";
+
 class Register extends React.Component{
 
-    handleChange(){
-        console.log('>>>>>>handleChangeEvent>>>>>>>>>>');
+    constructor(props){
+    
+        super(props);
+        this.state = {
+            username: '',
+            email: '',
+            password: '',
+            passwordConfirmation: '',
+        };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    // state = {
+    //     username: "",
+    //     email: "",
+    //     password: "",
+    //     passwordConfirmation: ""
+    //   };
+    handleChange(event){
+        console.log('>>>>>>handleChangeEvent>>>>>>>>>>',event.target.name,event.target.value);
+        this.setState({
+            [event.target.name ]: event.target.value
+        })
+    }
+
+    handleSubmit(event){
+        event.preventDefault();
+        console.log('>>>In handleSubmitHandler>>>>');
+        firebase
+        .auth()
+        .createUserWithEmailAndPassword(this.state.email, this.state.password)
+        .then( createdUser=>{
+            console.log('>>>createdUser>>>>>',createdUser);
+        }).catch(err=>{
+            console.log('>>>Exception>>>Occur>>>',err);
+        });
     }
 
     render(){
-        return(
+        const { username, email, password,passwordConfirmation  } = this.state;
 
+        return(
             <Grid textAlign= "center" verticalAlign="middle">
                 <Grid.Column style= {{ maxWidth:450 }} >
                     <Header as="h2" icon color="orange" textAlign="center">
@@ -18,28 +55,28 @@ class Register extends React.Component{
                     <Icon name="puzzle piece" color="orange"></Icon>
                     </Header>           
               
-                <Form size="large">
+                <Form  onSubmit={ this.handleSubmit } size="large">
             
                 <Segment stacked>
 
                 <Form.Input fluid name="username" icon="user" iconPosition="left" 
-                   placeholder="UserName" onChange= { this.handleChange } type="text" >
+                   placeholder="UserName" value= {username } onChange= { this.handleChange } type="text" >
                 </Form.Input>
 
                 <Form.Input fluid name="email" icon="mail" iconPosition="left" 
-                   placeholder="Email Address" onChange= { this.handleChange } type="email" >
+                   placeholder="Email Address" value= { email } onChange= { this.handleChange } type="email" >
                 </Form.Input>
 
                 <Form.Input fluid name="password" icon="lock" iconPosition="left" 
-                   placeholder="Password" onChange= { this.handleChange } type="password" >
+                   placeholder="Password" value={ password } onChange= { this.handleChange } type="password" >
                 </Form.Input>
 
 
                 <Form.Input fluid name="passwordConfirmation" icon="repeat" iconPosition="left" 
-                   placeholder="Password Confirmation" onChange= { this.handleChange } type="password" >
+                   placeholder="Password Confirmation" value={ passwordConfirmation } onChange= { this.handleChange } type="password" >
                 </Form.Input>
 
-                <Button color="orange" fluid size="large" > Submit </Button>
+                <Button color="orange" fluid size="large"  > Submit </Button>
                 </Segment>
                 </Form>
                 <Message>Already a User <Link to="/login">Login</Link></Message>
